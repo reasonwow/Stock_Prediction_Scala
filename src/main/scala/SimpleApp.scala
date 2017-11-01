8c5dc5cb4beb22cb68128de9d3533932c25c06d4
BOWEIWANG <kym1992@MBP.fios-router.home>
2017-10-31 23:55:22 -0400

    Spark Demo

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
+  }
+}
