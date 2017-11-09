9fb7f45546b386bbaaa7ff6a58b2126e1c91230b
BOWEIWANG <kym1992@MBP.local>
2017-11-08 19:09:34 -0500

    Update Yahoo API version

--- build.sbt
+++ build.sbt
@@ -5,3 +5,6 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+
+// https://mvnrepository.com/artifact/com.yahoofinance-api/YahooFinanceAPI
+libraryDependencies += "com.yahoofinance-api" % "YahooFinanceAPI" % "3.12.1"
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,13 +1,22 @@
 import org.apache.spark.sql.SparkSession
+import yahoofinance.Stock
+import yahoofinance.YahooFinance
+import java.util
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
-    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
-    val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
-    spark.stop()
+//    val logFile = "README.md" // Should be some file on your system
+//    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+//    val logData = spark.read.textFile(logFile).cache()
+//    val numAs = logData.filter(line => line.contains("a")).count()
+//    val numBs = logData.filter(line => line.contains("b")).count()
+//    println(s"Lines with a: $numAs, Lines with b: $numBs")
+//    spark.stop()
+    val symbols = Array[String]("INTC", "BABA", "TSLA", "AIR.PA", "YHOO")
+    val stocks = YahooFinance.get(symbols) // single request
+
+
+    val stock = YahooFinance.get("INTC")
+    stock.print()
   }
 }
