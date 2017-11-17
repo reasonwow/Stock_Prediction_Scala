0d4067a4f953a8c13bb04d04a05fab271a0e9c24
BOWEIWANG <kym1992@MBP.local>
2017-11-16 20:39:43 -0500

    time-series forcast

--- build.sbt
+++ build.sbt
@@ -4,7 +4,21 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
+
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.7"
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.7"
+dependencyOverrides += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.7"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.0"
+
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 
 // https://mvnrepository.com/artifact/com.yahoofinance-api/YahooFinanceAPI
 libraryDependencies += "com.yahoofinance-api" % "YahooFinanceAPI" % "3.12.1"
+
+// https://mvnrepository.com/artifact/com.cloudera.sparkts/sparkts
+libraryDependencies += "com.cloudera.sparkts" % "sparkts" % "0.4.1"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.11" % "2.2.0"
--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,22 +1,65 @@
+import java.time.{ZoneId, ZonedDateTime}
+
 import org.apache.spark.sql.SparkSession
 import yahoofinance.Stock
 import yahoofinance.YahooFinance
 import java.util
 
+import com.cloudera.sparkts.models.ARIMA
+import com.cloudera.sparkts.{DateTimeIndex, DayFrequency, TimeSeriesRDD}
+import org.apache.spark.mllib.linalg.DenseVector
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.functions.to_timestamp
+
 object SimpleApp {
   def main(args: Array[String]) {
-//    val logFile = "README.md" // Should be some file on your system
-//    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+    val logFile = "README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
 //    val logData = spark.read.textFile(logFile).cache()
 //    val numAs = logData.filter(line => line.contains("a")).count()
 //    val numBs = logData.filter(line => line.contains("b")).count()
 //    println(s"Lines with a: $numAs, Lines with b: $numBs")
 //    spark.stop()
-    val symbols = Array[String]("INTC", "BABA", "TSLA", "AIR.PA", "YHOO")
-    val stocks = YahooFinance.get(symbols) // single request
+//    val symbols = Array[String]("INTC")
+//    val stocks = YahooFinance.get(symbols) // single request
+//    val stock = YahooFinance.get("INTC")
+//    stock.print()
 
+    val formattedData = spark
+    .read
+    .option("header", "true")
+    .csv("/Users/kym1992/STUDY/NEU/CSYE7200/Dataset/nyse/prices-split-adjusted.csv")
+      .drop("open", "low", "high", "volume")
 
-    val stock = YahooFinance.get("INTC")
-    stock.print()
+    val finalDf = formattedData
+      .withColumn("timestamp", to_timestamp(formattedData("date"), "yyyy-MM-dd"))
+      .withColumn("price", formattedData("close").cast(DoubleType))
+      .drop("date","close").sort("timestamp")
+    finalDf.registerTempTable("preData")
+
+    val minDate = finalDf.selectExpr("min(timestamp)").collect()(0).getTimestamp(0)
+    val maxDate = finalDf.selectExpr("max(timestamp)").collect()(0).getTimestamp(0)
+    val zone = ZoneId.systemDefault()
+    val dtIndex = DateTimeIndex.uniformFromInterval(
+      ZonedDateTime.of(minDate.toLocalDateTime, zone),
+      ZonedDateTime.of(maxDate.toLocalDateTime, zone),
+      new DayFrequency(1)
+    )
+
+    val tsRdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, finalDf, "timestamp", "symbol", "price")
+
+    import spark.implicits._
+
+    val df = tsRdd.mapSeries
+    {vector => {
+      val newVec = new DenseVector(vector.toArray.map(x => if(x.equals(Double.NaN)) 0 else x))
+      val arimaModel = ARIMA.fitModel(1, 0, 0, newVec)
+      val forecasted = arimaModel.forecast(newVec, 5)
+      new DenseVector(forecasted.toArray.slice(forecasted.size-(5+1), forecasted.size-1))
+      }}.toDF("symbol","values")
+
+    df.registerTempTable("data")
+
+    df.collect.foreach(println)
   }
 }
