c1b537e6702dcc3e9a9117d43c1e47fab3601ca9
BOWEIWANG <kym1992@MBP.fios-router.home>
2017-11-20 17:47:24 -0500

    Optimize the NULL data

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -6,20 +6,14 @@ import yahoofinance.YahooFinance
 import java.util
 
 import com.cloudera.sparkts.models.ARIMA
-import com.cloudera.sparkts.{DateTimeIndex, DayFrequency, TimeSeriesRDD}
+import com.cloudera.sparkts.{BusinessDayFrequency, DateTimeIndex, DayFrequency, TimeSeriesRDD}
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.functions.to_timestamp
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "README.md" // Should be some file on your system
     val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
-//    val logData = spark.read.textFile(logFile).cache()
-//    val numAs = logData.filter(line => line.contains("a")).count()
-//    val numBs = logData.filter(line => line.contains("b")).count()
-//    println(s"Lines with a: $numAs, Lines with b: $numBs")
-//    spark.stop()
 //    val symbols = Array[String]("INTC")
 //    val stocks = YahooFinance.get(symbols) // single request
 //    val stock = YahooFinance.get("INTC")
@@ -43,10 +37,11 @@ object SimpleApp {
     val dtIndex = DateTimeIndex.uniformFromInterval(
       ZonedDateTime.of(minDate.toLocalDateTime, zone),
       ZonedDateTime.of(maxDate.toLocalDateTime, zone),
-      new DayFrequency(1)
+      new BusinessDayFrequency(1)
     )
 
-    val tsRdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, finalDf, "timestamp", "symbol", "price")
+    val tsRddToFill = TimeSeriesRDD.timeSeriesRDDFromObservations(dtIndex, finalDf, "timestamp", "symbol", "price")
+    val tsRdd = tsRddToFill.fill("previous")
 
     import spark.implicits._
 
