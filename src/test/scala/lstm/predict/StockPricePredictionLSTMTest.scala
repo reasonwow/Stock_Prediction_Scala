de52856ee8913829a8bed56955b4030a5937b0dd
BOWEIWANG <kym1992@MBP.fios-router.home>
2017-12-05 16:38:18 -0500

    add test cases

---
+++ src/test/scala/lstm/predict/StockPricePredictionLSTMTest.scala
@@ -0,0 +1,24 @@
+package lstm.predict
+
+import lstm.representation.PriceCategory
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class StockPricePredictionLSTMTest extends FlatSpec with Matchers with BeforeAndAfter {
+  behavior of "Test Prepare data in Stock Price Prediction LSTM"
+
+  it should "match the pattern as String, String, Double" in {
+    val result = StockPricePredictionLSTM.prepare("testData/testDataForSparkTS.csv", "GOOG", 1.0)
+    result._3 shouldBe 4.9E-324
+    result._4 shouldBe 1.7976931348623157E308
+    result._5 shouldBe PriceCategory.CLOSE
+  }
+
+  it should "match the size" in {
+    val preData = StockPricePredictionLSTM.prepare("testData/testDataForLSTM.csv", "WLTW", 0.90)
+    val testResult = StockPricePredictionLSTM.predictPriceOneAhead(preData._1, preData._2, preData._3, preData._4, preData._5)
+    testResult.actuals.size shouldBe 2
+    testResult.predicts.size shouldBe 2
+    testResult.actuals(0) shouldBe 122.37999725341797
+    testResult.predicts(0) shouldBe 124.44212476993579
+  }
+}
